4cf117637b7b4799407857b8230ad768d6b240bf
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2023-09-07 17:27:09 -0400

    Update q.java

--- q.java
+++ q.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class review
+{
+    private static Scanner myscanner = new Scanner(System.in);
+    public static void q4(){ 
+        double num = 121.234567;
+        double num2 = num * 10;
+        int num3 = (int)num2;
+        double num4 = (double)num3;
+        double ans = num4/10;
+        System.out.println(ans);
+    }
+    
+}
