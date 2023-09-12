6a8e2b61a7369032639c71ccbba5725ecada3628
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2023-09-12 12:08:36 -0400

    Update methods.java
    q1-3 not tested

--- methods.java
+++ methods.java
@@ -1,1 +1,28 @@
+public class methodsPratice
+{
+    public static void q1(String str){
+        String reverse = "";
+        char holder;
+        for (int i = 0; i< str.length(); i++){
+            holder = str.charAt(i);
+            reverse = holder + reverse;
+        }
+        System.out.println(reverse);
+    }
+    public static String q2(String str){
+        String reverse = "";
+        char holder;
+        for (int i = 0; i< str.length(); i++){
+            holder = str.charAt(i);
+            reverse = holder + reverse;
+        }
+        return reverse;
+    }
+    public static void q3(int length, int width){
+        int area = length * width;
+        int perimeter = 2*(length+width);
+        System.out.println("Area: "+area+"Perimeter: "+perimeter);
+    }
+}
+
 
