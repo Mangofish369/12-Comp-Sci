b8a147cf04ba955cc3d8a085a0061a25fe4402f0
Mangofish369 <117941655+Mangofish369@users.noreply.github.com>
2023-09-12 20:46:01 -0400

    q4 added
    - Tested q3 (workd)
    - Added q4 as well

--- methods.java
+++ methods.java
@@ -1,4 +1,4 @@
-public class methodsPratice
+public class methods
 {
     public static void q1(String str){
         String reverse = "";
@@ -21,7 +21,19 @@ public class methodsPratice
     public static void q3(int length, int width){
         int area = length * width;
         int perimeter = 2*(length+width);
-        System.out.println("Area: "+area+"Perimeter: "+perimeter);
+        System.out.println("Area: "+area);
+        System.out.println("Perimeter: "+perimeter);
+    }
+    public static int q4 (int nums[]){
+        boolean even = false;
+        int lowest = -1;
+        for (int i : nums){
+            if (i %2 == 0 && even == false || i < lowest){
+                lowest = i;
+                even = true;
+            }
+        }
+        return lowest;
     }
 }
 
