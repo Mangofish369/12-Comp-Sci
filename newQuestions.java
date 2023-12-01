b3cc517a351953e2f261188df72e887212e2a48c
Mangofish369 <340868470@gapps.yrdsb.ca>
2023-11-30 22:18:19 -0500

    Question 2 Complete and is now functional

--- newQuestions.java
+++ newQuestions.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.Arrays; 
 import java.util.Scanner;
+import java.lang.ArrayIndexOutOfBoundsException;
 public class newQuestions
 {
     public static void Question1 (){
@@ -35,38 +36,70 @@ public class newQuestions
         int height = 5;
         int [][] map = new int [width][height];
         Scanner input = new Scanner (System.in);
-     
+        
+        populate2dArray(map,0);
         map[0][0] = 1;
         int [] currCordinate = {0,0}; 
-        print2dArray(map);
+        print2dArray(map, width, height);
         
         System.out.println("Please enter a direction (L, R, U, D) or Stop: ");
         String direction = input.nextLine(); 
-        while(direction != "Stop"){
-            print2dArray(map);
-            
+        while(!direction.equals ("Stop")){
+            System.out.println(direction);
+            try{
+                move(direction, map, currCordinate);
+                print2dArray(map, width, height);
+            }
+            catch(ArrayIndexOutOfBoundsException e) {
+                System.out.println("Invalid – you cannot move in that direction from your current position");
+            }
             System.out.println("Please enter a direction (L, R, U, D) or Stop: ");
             direction = input.nextLine(); 
         }
     }
     
+    /**
+     * 2D arrays function in a way where x-cordinates represent coloums and y-coordinates represent rows
+     * To work around this, the x and y values must be switched around
+     */
     public static void move(String direction, int [][] array, int [] coordinate){
-        // Find a way to return the new coordinate
         int x = coordinate[0];
         int y = coordinate [1];
         if(direction.equals("L")){
-            array[x][y] = 0;
-            array[x-1][y] = 1;
+            array[y][x] = 0;
+            array[y][x-1] = 1;
+            coordinate = updateCord(coordinate,x-1,y); 
         } else if(direction.equals("R")){
-            array[x][y] = 0;
-            array[x+1][y] = 0;
+            array[y][x] = 0;
+            array[y][x+1] = 1;
+            coordinate = updateCord(coordinate,x+1,y); 
+        } else if(direction.equals ("U")){
+            array[y][x] = 0;
+            array[y-1][x] = 1;
+            coordinate = updateCord(coordinate,x,y-1); 
+        } else if(direction.equals ("D")){
+            array[y][x] = 0;
+            array[y+1][x] = 1;
+            coordinate = updateCord(coordinate,x,y+1); 
         }
     }
     
-    public static void print2dArray(int [][] array){
+    public static void populate2dArray(int [][] array, int value){
         for(int i = 0; i <5; i++){
             for(int j = 0; j < 5; j++){
-                array[i][j] = 0;
+                array[i][j] = value;
+            }
+        }
+    }
+    public static int[] updateCord(int [] coordinate, int x, int y){
+        coordinate[0] = x;
+        coordinate[1] = y;
+        return coordinate;
+    }
+    
+    public static void print2dArray(int [][] array, int width, int height){
+        for(int i = 0; i <height; i++){
+            for(int j = 0; j < width; j++){
                 System.out.print(array[i][j] + "\t");
             }
             System.out.print("\n");
